c57c59d7ae3474bdb83aa1d802f4e35bcf439860
squirrel <seo.squirrel@yandex.ru>
2019-05-09 17:32:53 +0300

    опечатку убрал

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ gulp.task('sass-compile', function () {
 	return gulp.src('./scss/**/main.scss')
 	.pipe(sourcemaps.init())
 	.pipe(sass().on('error', sass.logError))
-	.pipe(sourcemaps.write('./'))W
+	.pipe(sourcemaps.write('./'))
 	.pipe(gulp.dest('./css/'));
 });
 
