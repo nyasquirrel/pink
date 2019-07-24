692c160094dc71981fd87ae76ca9516f1545254d
nyasquirrel <seo.squirrel@yandex.ru>
2019-07-24 18:00:16 +0300

    пытаюсь сделать карту

--- src/js/script.js
+++ src/js/script.js
@@ -13,3 +13,17 @@ burger.addEventListener('click', function (event) {
   header.classList.toggle('header--closed');
   header.classList.toggle('header--opened');
 });
+
+
+ymaps.ready(function () {
+  var map = new ymaps.Map("map", {
+    center: [55.76, 37.64],
+    zoom: 10
+  });
+
+  if (map) {
+    ymaps.modules.require(['Placemark', 'Circle'], function (Placemark, Circle) {
+      var placemark = new Placemark([55.37, 35.45]);
+    });
+  }
+});
